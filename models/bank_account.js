dc2f5fd963f4c91434aaec56f8d42fb0991d5eb8
VBaisas <baisas.v@gmail.com>
2020-08-29 18:45:13 -0600

    Add Bank account schema

---
+++ models/bank_account.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var bankAccountSchema = new Schema({
+   name: {
+       type: String,
+       required: [true, 'Account name is required.'],
+   },
+   type: {
+       type: String,
+       required: [true, 'Account type is required.']
+   }
+   ,
+   balance: {
+       type: Number,
+       required: [true, 'Balance is required.']
+   }
+}
+
+var bankAccount = mongoose.model('User', userSchema);
+
+module.exports = bankAccount;
