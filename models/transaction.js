96529117ad397ea4aeed6da472f824b382e9d3cf
VBaisas <baisas.v@gmail.com>
2020-09-06 19:36:49 -0600

    Define 'Transaction' model

---
+++ models/transaction.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const transactionSchema = new Schema({
+    account: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Account'
+    },
+    category: {
+       type: String,
+       required: [true, 'Category is required.'],
+    },
+    Date: {
+       type: Date,
+       required: [true, 'Transaction date is required.']
+    },
+    amount: {
+       type: Number,
+       required: [true, 'Transaction amount is required.']
+    },
+    description: {
+        type: String
+     }
+});
+
+const Transaction = mongoose.model('Transaction', transactionSchema);
+
+module.exports = Transaction;
