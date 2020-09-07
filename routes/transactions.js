214567b81172887988d08c22964de3e99430e41c
VBaisas <baisas.v@gmail.com>
2020-09-06 20:51:23 -0600

    Add router for '/transactions'

--- routes/transactions.js
+++ routes/transactions.js
@@ -3,6 +3,11 @@ var transactionController = require('../controllers/transactions.js');
 var router = express.Router();
 
 router 
+  .route('/transactions')
+  .get(transactionController.transactionsInputForm)
+  .post(transactionController.create)
+
+router 
   .route('/transactions/expenses')
   .get(transactionController.transactionsInputForm)
   .post(transactionController.create)
