63ce46a48684c157e6a3e64228c18dcf583ea182
VBaisas <baisas.v@gmail.com>
2020-09-12 23:08:28 -0600

    Add _id add Account.find function to accountsInputForm

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -1,28 +1,36 @@
 var express = require('express');
 var Account = require('../models/account');
+const mongoose = require('mongoose');
 
 exports.accountsInputForm = function(req, res) {
-    res.render('accounts/manage_accounts', { account: {}, errors: [] });
+  Account.find(function (err, accounts) {
+    if (err) console.log(err)
+    res.render('accounts/manage_accounts', { accounts: accounts, title: 'Lolly | Accounts', errors: [] });
+  })
 };
 
 exports.create = function(req, res) {
-    var type = req.body.type;
-    var description = req.body.description;
-    var balance = req.body.balance;
+  var _id = new mongoose.Types.ObjectId();
+  var type = req.body.type;
+  var description = req.body.description;
+  var balance = req.body.balance;
 
-    var newAccount = new Account({
-      description: description,
-      type: type,
-      balance: balance
-    });
+  var newAccount = new Account({
+    _id: _id,
+    description: description,
+    type: type,
+    balance: balance
+  });
 
-    newAccount.save(function(err) {
-      if (err) {
-          res.render('accounts/manage_accounts', { account: newAccount, errors: err.errors });
-      } else {
-          res.redirect('/');
-          console.log('Account saved successfully!');
-      }
+  newAccount.save(function(err) {
+    if (err) {
+        res.render('accounts/manage_accounts', { account: newAccount, errors: err.errors });
+        console.log(err)
+    } else {
+        res.redirect('/');
+        console.log('Account saved successfully!');
+    }
 
-    });
+  });
 };
+
