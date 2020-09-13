eebe26a02ff59c168597f3dbe0a10377fc0df9fe
VBaisas <baisas.v@gmail.com>
2020-09-12 23:11:29 -0600

    Add reference to _id from Accounts

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var Transaction = require('../models/transaction');
+var Account = require('../controllers/accounts');
 
 exports.expensesInputForm = function(req, res) {
     res.render('transactions/expenses', { transaction: {}, errors: [] });
@@ -9,7 +10,7 @@ exports.incomeInputForm = function(req, res) {
   res.render('transactions/income', { transaction: {}, errors: [] });
 };
 
-exports.create = function(req, res) {
+/*exports.create = function(req, res) {
     var account = req.body.account;
     var category = req.body.category;
     var date = req.body.date;
@@ -39,4 +40,36 @@ exports.create = function(req, res) {
       }
 
     });
+};*/
+
+exports.create = function(req, res) {
+  var account = Account.create._id;
+  var category = req.body.category;
+  var date = req.body.date;
+  var amount = req.body.amount;
+  var description = req.body.description;
+  var type = req.body.type;
+
+  var newTransaction = new Transaction({
+    account: account,
+    category: category,
+    date: date,
+    amount: amount,
+    description: description,
+    type: type
+  });
+
+  newTransaction.save(function(err) {
+    if (err) {
+        res.redirect('/transactions');
+        console.log(err);
+    } else if (type === 'Income') {
+        res.redirect('/transactions/income');
+        console.log('Transaction saved successfully!');          
+    } else {
+        res.redirect('/transactions/expenses');
+        console.log('Transaction saved successfully!');
+    }
+
+  });
 };
