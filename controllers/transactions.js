f376eec4ee12c0bc23304f1bdff4b50f0fcffc4b
VBaisas <baisas.v@gmail.com>
2020-09-13 15:12:16 -0600

    Update expensesInputForm and incomeInputForm to list accounts

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,49 +1,27 @@
 var express = require('express');
 var Transaction = require('../models/transaction');
-var Account = require('../controllers/accounts');
+var Account = require('../models/account');
 
-exports.expensesInputForm = function(req, res) {
-    res.render('transactions/expenses', { transaction: {}, errors: [] });
+exports.expensesInputForm = function(req, res, next) {
+
+  Account.find({},'description')
+ .exec(function (err, accounts) {
+   if (err) { return next(err); }
+   res.render('transactions/expenses', {title: 'Lolly | Transactions | Expenses', accountList: accounts } );
+ });
 };
 
-exports.incomeInputForm = function(req, res) {
-  res.render('transactions/income', { transaction: {}, errors: [] });
+exports.incomeInputForm = function(req, res, next) {
+
+  Account.find({},'description')
+ .exec(function (err, accounts) {
+   if (err) { return next(err); }
+   res.render('transactions/income', {title: 'Lolly | Transactions | Income', accountList: accounts } );
+ });
 };
-
-/*exports.create = function(req, res) {
-    var account = req.body.account;
-    var category = req.body.category;
-    var date = req.body.date;
-    var amount = req.body.amount;
-    var description = req.body.description;
-    var type = req.body.type;
-
-    var newTransaction = new Transaction({
-      account: account,
-      category: category,
-      date: date,
-      amount: amount,
-      description: description,
-      type: type
-    });
-
-    newTransaction.save(function(err) {
-      if (err) {
-          res.redirect('/transactions');
-          console.log(err);
-      } else if (type === 'Income') {
-          res.redirect('/transactions/income');
-          console.log('Transaction saved successfully!');          
-      } else {
-          res.redirect('/transactions/expenses');
-          console.log('Transaction saved successfully!');
-      }
-
-    });
-};*/
 
 exports.create = function(req, res) {
-  var account = Account.create._id;
+  var account = req.body.account;
   var category = req.body.category;
   var date = req.body.date;
   var amount = req.body.amount;
