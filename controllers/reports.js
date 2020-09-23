149fd2a9900154abc3776ce55f2db2a0924608f1
VBaisas <baisas.v@gmail.com>
2020-09-22 22:36:57 -0600

    Add chart of spendings

--- controllers/reports.js
+++ controllers/reports.js
@@ -4,9 +4,15 @@ var Transaction = require('../models/transaction');
 
 exports.spendingOverTime = function(req, res) {
   
-  Transaction.find(function (err, transactions) {
+  Transaction.aggregate([
+    { $match : { type : "Expense" } },
+    {$group: {_id: '$date', 'transactionAmount': {$sum: { $multiply: [ -1,'$amount'] }}}},
+    {$sort: { _id: 1 }},
+    {$group: { _id: null, 'date': {$push: '$_id'}, 'transactionAmount': {$push: '$transactionAmount'}}}
+  ], (function (err, transactionsAggregate) {
         if (err) console.log(err)
-        res.render('reports/spending_over_time', { transactions: transactions, title: 'Lolly | Reports' });
-    });
+        res.render('reports/spending_over_time', { transactionsAggregate: transactionsAggregate, title: 'Lolly | Reports' });
+        console.log(transactionsAggregate[0].date, transactionsAggregate[0].transactionAmount);
+    }));
     
 };
